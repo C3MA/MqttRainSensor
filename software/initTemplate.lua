786b86dc321f67ebe12548201688fc7b7c15b7f5
ollo <ollo@olloThinkPad>
2017-11-12 20:49:21 +0100

    Included a delay into the init template

--- software/initTemplate.lua
+++ software/initTemplate.lua
@@ -4,5 +4,8 @@ wifi.sta.config("SSID","PASSWORD")
 -- All global Variables
 sollich=1
 maintenanceMode=0
-
-dofile("main.lua")
+-- Use: tmr.stop(6) to abort
+print("Autostart in 5 Seconds. ")
+tmr.alarm(6, 5000, 1, function()
+    dofile("main.lua")
+end)
